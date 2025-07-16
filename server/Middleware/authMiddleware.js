beed94d40cdedd432c927017688c706c26faf35b
comrade-047 <amitkumar.4.7.09.27@gmail.com>
2025-07-16 14:01:11 +0530

    Handle the bug when no token is passed

--- server/Middleware/authMiddleware.js
+++ server/Middleware/authMiddleware.js
@@ -3,15 +3,20 @@ import User from "../models/user.js";
 
 // for securing the routes like dashboard, submit etc.
 export const protect = async(req, res, next) => {
-    let token = req.headers.authorization.split(' ')[1];
-
-    if(!token) {
-        return res.status(401).json({ message: 'Not authorized, no token' });
-    }
 
     try{
+        const authHeader = req.headers.authorization;
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            return res.status(401).json({message : "Not authorized , no token"});
+        }
+        let token = authHeader.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id);
+
+        const user = await User.findById(decoded.id);
+        if(!user) {
+            return res.status(401).json({message : "Not authorized ,user not found"});
+        }
+        req.user = user;
         next();
     }
     catch(error){
